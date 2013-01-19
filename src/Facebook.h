3a35c9609851799842e0fb03b8a94e95c6f617b3
Kacimo <bk4a-.-@hotmail.com>
2013-01-19 15:13:08 +0100

    commit number 3

--- src/Facebook.h
+++ src/Facebook.h
@@ -1,5 +1,6 @@
 /*
  * Copyright 2010 Facebook == waste of time and money
+ * Judaisme
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
